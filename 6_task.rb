23bfb50e6e6cca8930ba2ab960c8f572a530e240
Alla Kostyk <alla.kostyk.if@gmail.com>
2020-02-06 19:10:40 +0200

    modified_6_7_8

--- 6_task.rb
+++ 6_task.rb
@@ -1,7 +1,28 @@
 def first_and_last(array)
-  array.values_at(0,-1)
+  last_element = array.last
+  first_element = array.first
+  index_array = []
+  array.each_with_index do |element, index|
+    index_array << index if first_element < element && element < last_element
+  end
+  puts index_array.last
 end
 
-a = [2,1,47,3,99,9]
+array = [2,3,47,5,7,9]
 
-first_and_last(a)
+first_and_last(array)
+#Puts the index of the last element
+
+def first_and_last(array)
+  last_element = array.last
+  first_element = array.first
+
+  array.each_with_index do |element, index|
+    return index if first_element < element && element < last_element
+  end
+end
+
+array = [2,3,47,5,7,9]
+
+first_and_last(array)
+##Puts the index of the first element
